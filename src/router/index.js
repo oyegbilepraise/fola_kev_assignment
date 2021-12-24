c9b7cd0ce74191384c97c64d6b07d2285de8f815
Praise <oluwadhammieh@gmail.com>
2021-12-24 22:35:04 +0100

    pushed for kev & fola

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter, createWebHistory } from "vue-router";
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard'
+import Balance from '../views/Balance'
+// import Praise from '../views/Praise'
+
+
+const routes = [
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/dashboard/:id",
+    name: "Dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/balance",
+    name: "Balance",
+    component: Balance,
+  },
+  // {
+  //   path: "/praise",
+  //   name: "Praise",
+  //   component: Praise,
+  // },
+  {
+    path: "/",
+    name: "Login",
+    component: Login,
+  },
+  // {
+    // path: "/about",
+    // name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+      // import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  // },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
